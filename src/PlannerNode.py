451f64087b65f91740af88afdc4fab092940ef6a
Tao Yao <yaotaonb123@gmail.com>
2020-12-11 16:23:07 -0800

    PlannerNode first prototype for final (pseudo code only)

--- src/PlannerNode.py
+++ src/PlannerNode.py
@@ -14,6 +14,7 @@ from HaltonEnvironment import HaltonEnvironment
 import GraphGenerator
 import Utils
 
+import csv_handler
 
 
 class PlannerNode(object):
@@ -27,7 +28,8 @@ class PlannerNode(object):
                      pub_topic,
                      service_topic,
                      car_width,
-                     car_length):
+                     car_length,
+                     pose_arr):
     
     print("[Planner Node] Getting map from service...")
     rospy.wait_for_service(map_service_name)
@@ -52,6 +54,8 @@ class PlannerNode(object):
     
     self.cur_plan = None
     self.plan_lock = Lock()
+
+    self.pose_arr = pose_arr
     
     self.orientation_window_size = 21
     print('pub topic: ' + pub_topic)
@@ -163,21 +167,13 @@ class PlannerNode(object):
       oriented_plan[:,2] = np.arctan2(plan[1,1]-plan[0,1],plan[1,0]-plan[0,0])
 
     return oriented_plan
-      
-  def update_plan(self):
-    self.source_lock.acquire()
-    self.target_lock.acquire()
-    if self.source_pose is not None:
-      source_pose = np.array(self.source_pose).reshape(2)
-    if self.target_pose is not None:
-      target_pose = np.array(self.target_pose).reshape(2)
-    replan = ((self.source_updated or self.target_updated) and
-              (self.source_pose is not None and self.target_pose is not None))
-    self.source_updated = False
-    self.target_updated = False
-    self.source_lock.release()
-    self.target_lock.release()
-    if replan:
+  
+  def final_plan(self):
+    final_plan = []
+    for i in range(0, len(self.pose_arr)-1):
+      source_pose = self.pose_arr[i]
+      target_pose = self.pose_arr[i+1]
+
       if(np.abs(source_pose-target_pose).sum() < sys.float_info.epsilon):
         print '[Planner Node] Source and target are the same, will not plan'
         return
@@ -190,23 +186,95 @@ class PlannerNode(object):
         print '[Planner Node] Target in collision, will not plan'
         return
 
-      print '[Planner Node] Inserting source and target'
+      print('[Planner Node] Inserting source and target, source index: ', i, '; target index: ', i+1, 'pose_arr len: ', len(pose_arr))
       self.environment.set_source_and_target(source_pose, target_pose)
 
       print '[Planner Node] Computing plan...'
-      self.cur_plan = self.planner.plan()    
+      temp_plan = self.planner.plan()    
       
-      if self.cur_plan is not None:
-        # self.cur_plan = self.planner.post_process(self.cur_plan, 5)
-        self.cur_plan = self.add_orientation(self.cur_plan)
-        print '[Planner Node] ...plan complete'
+      if temp_plan is not None:
+        temp_plan = self.planner.post_process(temp_plan, 5)
+        temp_plan = self.add_orientation(temp_plan)
+        print('[Planner Node] plan segment at index: ', i, ' complete')
       else:
-        print '[Planner Node] ...could not compute a plan'
+        print('[Planner Node] plan segment at index: ', i, ' could not compute a plan')
+
+      final_plan.append(temp_plan)
+
+    #after csv_handler and map_gen is finished, check what array type (np array or py list?) is self.cur_plan so that self.cur_plan is a whole plan array in the end
+    for i in final_plan:
+      for j in i:
+        self.cur_plan.append(j)
+
+    if (self.cur_plan is not None) and (self.plan_pub is not None): #this step is only for visualization of the final plan in rviz
+      self.publish_plan(self.cur_plan)
+
+
+  # def update_plan(self): #remove this function after everything runs fine
+  #   self.source_lock.acquire()
+  #   self.target_lock.acquire()
+  #   if self.source_pose is not None:
+  #     source_pose = np.array(self.source_pose).reshape(2)
+  #   if self.target_pose is not None:
+  #     target_pose = np.array(self.target_pose).reshape(2)
+  #   replan = ((self.source_updated or self.target_updated) and
+  #             (self.source_pose is not None and self.target_pose is not None))
+  #   self.source_updated = False
+  #   self.target_updated = False
+  #   self.source_lock.release()
+  #   self.target_lock.release()
+  #   if replan:
+  #     if(np.abs(source_pose-target_pose).sum() < sys.float_info.epsilon):
+  #       print '[Planner Node] Source and target are the same, will not plan'
+  #       return
+
+  #     if not self.environment.manager.get_state_validity(source_pose):
+  #       print '[Planner Node] Source in collision, will not plan'
+  #       return
+
+  #     if not self.environment.manager.get_state_validity(target_pose):
+  #       print '[Planner Node] Target in collision, will not plan'
+  #       return
+
+  #     print '[Planner Node] Inserting source and target'
+  #     self.environment.set_source_and_target(source_pose, target_pose)
+
+  #     print '[Planner Node] Computing plan...'
+  #     self.cur_plan = self.planner.plan()    
+      
+  #     if self.cur_plan is not None:
+  #       self.cur_plan = self.planner.post_process(self.cur_plan, 5)
+  #       self.cur_plan = self.add_orientation(self.cur_plan)
+  #       print '[Planner Node] ...plan complete'
+  #     else:
+  #       print '[Planner Node] ...could not compute a plan'
       
 
-    if (self.cur_plan is not None) and (self.plan_pub is not None):
-      self.publish_plan(self.cur_plan)  
+  #   if (self.cur_plan is not None) and (self.plan_pub is not None):
+  #     self.publish_plan(self.cur_plan)  
     
+# a or b are np.array with 2 elements like this: [x, y]
+def L2dist(a, b): #return L2 distance between a and b
+  return np.linalg.norm(a-b)
+
+def get_pose_arr(start_point, good_points):
+  pose_arr = []
+  pose_arr.append(start_point)
+  pool = good_points[:]
+  cur = start_point[:]
+  while pool: #brute force search keep finding the next nearest point until pool is empty
+    mindist = 99999.99 #the current min dist
+    minidx = 0
+    for i in range(len(pool)):
+      dist = min(L2dist(cur, pool[i]), mindist)
+      if dist < mindist:
+        minidx = i
+        mindist = dist
+    cur = pool[minidx]
+    pose_arr.append(pool[minidx])
+    del pool[minidx]
+  return pose_arr #not sure about numpy to list or vice versa conversion yet but it depends on if planner takes np.array or a list, finish this later
+
 if __name__ == '__main__':
   rospy.init_node('planner_node', anonymous=True)
   
@@ -221,6 +289,11 @@ if __name__ == '__main__':
   car_width = rospy.get_param("/car_kinematics/car_width", 0.33)
   car_length = rospy.get_param("/car_kinematics/car_length", 0.33)
 
+  csv_path = '../way_points/real_car/'
+  start_point = csv_handler.get_waypoint_start(csv_path)
+  good_points = csv_handler.get_waypoint_good(csv_path)
+  pose_arr = get_pose_arr(start_point, good_points)
+
   pn = PlannerNode(map_service_name, 
                    halton_points, 
                    disc_radius,
@@ -230,7 +303,8 @@ if __name__ == '__main__':
                    pub_topic,
                    service_topic,
                    car_width,
-                   car_length)
+                   car_length,
+                   pose_arr)
                    
   while not rospy.is_shutdown():
     if pub_topic is not None:
